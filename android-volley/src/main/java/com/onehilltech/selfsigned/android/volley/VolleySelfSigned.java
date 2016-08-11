0450f7f18e9eb3887198de1df535b2943d2f6333
James H. Hill <hilljh82@gmail.com>
2016-08-11 12:10:47 -0400

    Refactored non-Volley logic into its own project

--- android-volley/src/main/java/com/onehilltech/selfsigned/android/volley/VolleySelfSigned.java
+++ android-volley/src/main/java/com/onehilltech/selfsigned/android/volley/VolleySelfSigned.java
@@ -5,20 +5,16 @@ import android.content.Context;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.Volley;
+import com.onehilltech.selfsigned.android.AndroidSelfSigned;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
+
 
 /**
  * Volley implementation support for self trust.
@@ -67,34 +63,8 @@ public final class VolleySelfSigned
   public static RequestQueue newRequestQueue (Context context, String type, String assetFile)
       throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException
   {
-    CertificateFactory cf = CertificateFactory.getInstance (type);
-    InputStream caInput = new BufferedInputStream (context.getAssets ().open (assetFile));
-
-    try
-    {
-      Certificate ca = cf.generateCertificate (caInput);
-
-      // Create a KeyStore containing our trusted CAs
-      String keyStoreType = KeyStore.getDefaultType ();
-      KeyStore keyStore = KeyStore.getInstance (keyStoreType);
-      keyStore.load (null, null);
-      keyStore.setCertificateEntry ("ca", ca);
-
-      // Create a TrustManager that trusts the CAs in our KeyStore
-      String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm ();
-      TrustManagerFactory tmf = TrustManagerFactory.getInstance (tmfAlgorithm);
-      tmf.init (keyStore);
-
-      // Create an SSLContext that uses our TrustManager
-      SSLContext sslContext = SSLContext.getInstance (DEFAULT_SSL_CONTEXT_TYPE);
-      sslContext.init (null, tmf.getTrustManagers (), null);
-
-      HurlStack hurlStack = new HurlStack (null, sslContext.getSocketFactory ());
-      return Volley.newRequestQueue (context, hurlStack);
-    }
-    finally
-    {
-      caInput.close ();
-    }
+    SSLContext sslContext = AndroidSelfSigned.newSSLContext (context, type, assetFile);
+    HurlStack hurlStack = new HurlStack (null, sslContext.getSocketFactory ());
+    return Volley.newRequestQueue (context, hurlStack);
   }
 }
---
+++ android/src/main/java/com/onehilltech/selfsigned/android/AndroidSelfSigned.java
@@ -0,0 +1,62 @@
+package com.onehilltech.selfsigned.android;
+
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+public class AndroidSelfSigned
+{
+  public static final String DEFAULT_CA_TYPE = "X.509";
+  public static final String DEFAULT_SSL_CONTEXT_TYPE = "TLS";
+
+  public static SSLContext newSSLContext (Context context, String assetFile)
+      throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException
+  {
+    return newSSLContext (context, DEFAULT_CA_TYPE, assetFile);
+  }
+
+  public static SSLContext newSSLContext (Context context, String type, String assetFile)
+      throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException
+  {
+    CertificateFactory cf = CertificateFactory.getInstance (type);
+    InputStream caInput = new BufferedInputStream (context.getAssets ().open (assetFile));
+
+    try
+    {
+      Certificate ca = cf.generateCertificate (caInput);
+
+      // Create a KeyStore containing our trusted CAs
+      String keyStoreType = KeyStore.getDefaultType ();
+      KeyStore keyStore = KeyStore.getInstance (keyStoreType);
+      keyStore.load (null, null);
+      keyStore.setCertificateEntry ("ca", ca);
+
+      // Create a TrustManager that trusts the CAs in our KeyStore
+      String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm ();
+      TrustManagerFactory tmf = TrustManagerFactory.getInstance (tmfAlgorithm);
+      tmf.init (keyStore);
+
+      // Create an SSLContext that uses our TrustManager
+      SSLContext sslContext = SSLContext.getInstance (DEFAULT_SSL_CONTEXT_TYPE);
+      sslContext.init (null, tmf.getTrustManagers (), null);
+
+      return sslContext;
+    }
+    finally
+    {
+      caInput.close ();
+    }
+  }
+}
